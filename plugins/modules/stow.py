6a481634910f84ae60f3fc706d08a8bffb07b8ec
dandyrow <development@daniellowry.co.uk>
2023-02-10 17:02:59 +0000

    Added python files for module code & unit tests
    Added directory structure & __init__.py for python module discovery.
    Added test/outputs to gitignore

---
+++ plugins/modules/stow.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+"""Ansible module to interact with GNU stow utility"""
+# Copyright (C) 2023 Daniel Lowry <development@daniellowry.co.uk>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation under version 3 of the License.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+def main():
+    # type: () -> None
+    """runs Ansible stow module"""
+    return
+
+
+if __name__ == '__main__':
+    main()
